a29c454d501f9119a4cd0531c49e95df693afddd
curiouscosmos <onecuriouscosmos@gmail.com>
2024-11-25 14:07:08 -0800

    Release v118.0.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/talkshoplive/webrtc/releases/download/v118.0.0/WebRTC.xcframework.zip",
+            url: "https://github.com/TalkShopLive/webrtc/releases/download/v118.0.0/WebRTC.xcframework.zip",
             checksum: "d2548ea930ee135ea8269041c34031f388014d2b2c8aa5f7a7cfb892b603c31c"
         ),
     ]
