89168f0aae909df7151157a500cac59fb71d8852
curiouscosmos <onecuriouscosmos@gmail.com>
2024-11-25 14:04:24 -0800

    Release v118.0.1

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "JitsiWebRTC",
+    name: "TalkShopLiveWebRTC",
     platforms: [.iOS(.v12)],
     products: [
         .library(
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/jitsi/webrtc/releases/download/v124.0.1/WebRTC.xcframework.zip",
+            url: "https://github.com/talkshoplive/webrtc/releases/download/v118.0.1/WebRTC.xcframework.zip",
             checksum: "d2548ea930ee135ea8269041c34031f388014d2b2c8aa5f7a7cfb892b603c31c"
         ),
     ]
