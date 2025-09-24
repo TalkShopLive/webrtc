fed819cab814ec8e10df49774c844a343357a970
daman-talkshoplive <112784890+daman-talkshoplive@users.noreply.github.com>
2025-09-24 14:04:18 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/TalkShopLive/webrtc/releases/download/v124.0.2/WebRTC.xcframework.zip",
+            url: "https://github.com/TalkShopLive/webrtc/releases/download/v124.0.3/WebRTC.xcframework.zip",
             checksum: "a5fc15bd2547e9e282e3756885c76fe78a223082e0f6a767a03791ea525f0e2e"
         ),
     ]
